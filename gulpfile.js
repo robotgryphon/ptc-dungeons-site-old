259c4ea1491d74551a554a9e7d1846f9a3623ad7
Ted Senft <code@robotgryphon.com>
2018-03-28 02:05:54 -0400

    Add missing pages as WIP, gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,7 @@ paths.final.styles =    path.join(paths.final.root, "styles");
 
 gulp.task("build", ["build::copy", "build::styles"]);
 
-gulp.task("build::copy", () => {
-    let stream = gulp.src(paths.pages + '/**/*.html')
-        .pipe(gulp.dest(paths.final.pages));
-
-    let media = gulp.src(paths.media + "/**/*")
-        .pipe(gulp.dest(paths.final.media));
-  });
+gulp.task("build::copy", ["copy::pages", "copy::media"]);
 
 gulp.task("build::styles", () => {
   let stream = gulp.src(paths.styles + '/**/*.scss')
@@ -30,4 +24,26 @@ gulp.task("build::styles", () => {
     .pipe(gulp.dest(paths.final.styles));
 
   return stream;
+});
+
+gulp.task("copy::pages", () => {
+    let stream = gulp.src(paths.pages + '/**/*.html')
+        .pipe(gulp.dest(paths.final.pages));
+    return stream;
+});
+
+gulp.task("copy::media", () => {
+    let media = gulp.src(paths.media + "/**/*")
+        .pipe(gulp.dest(paths.final.media));
+    return media;    
+});
+
+gulp.task("watch", ["watch::styles", "watch::pages"]);
+
+gulp.task("watch::styles", () => {
+    gulp.watch(path.join(paths.styles, "**/*.scss"), ["build::styles"]);
+});
+
+gulp.task("watch::pages", () => {
+    gulp.watch(path.join(paths.pages, "**/*.html"), ["copy::pages"]);
 });
